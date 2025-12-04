b75b1dcbeeada5a4aba174ac826b21c533666f15
baqermo <115046890+baqermo@users.noreply.github.com>
2025-12-04 17:08:47 +0000

    saving work

---
+++ pizza.py
@@ -0,0 +1,32 @@
+# Displays the options 1-4 when program is executed, each with their own function.
+def order_pizza():
+    #code here for programming lead
+def view_cart():
+     #code here for programming lead
+def checkout():
+     #code here for programming lead
+def main_menu();
+    while True:
+        print("PIZZA ORDERING MENU")
+        print("1. Order Pizza")
+        print("2. View Cart")
+        print("3. Checkout")
+        print("4. Exit")
+    """
+    Displays the menu with number choices to pick from, user must pick numbers 1-4
+    or they will get a message prompting them to pick from the options. based on the number
+    they pick, they will get the menu of pizzas, checkout, see their cart, or exit the menu.
+    """
+    choice = input("Choose an option 1-4")
+    if choice =="1":
+        order_pizza()
+    elif choice =="2":
+        view_cart()
+    elif choice =="3":
+        Checkout()
+    elif choice =="4"
+        break
+    else:
+        print("Please pick a Choice from 1 to 4")
+
+        
