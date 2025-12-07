3f4f4cd5941135cf96194f97ab54c8c0e60e6b79
Connor418 <cstafford10@hawkmail.hccfl.edu>
2025-12-07 17:51:06 -0500

    written most of code out (pizza.py)
    Most of the code should be written out, however i dont know how to get the functions to work

--- pizza.py
+++ pizza.py
@@ -1,12 +1,137 @@
 # Displays the options 1-4 when program is executed, each with their own function.
 def order_pizza():
-    #code here for programming lead
+    # setting variables to check if small, medium, or large is selected
+    Small = False
+    Medium = False
+    Large = False
+    SExit = False
+    # Displaying prices of pizza
+    print("Small = 5.99")
+    print("Medium = 7.99")
+    print("Large = 9.99")
+    # Loop for selecting the pizza size
+    while SExit == False:
+        size = input("What size would you like? (Type S, M, or L): ")
+        if input == 'S':
+            Small = True
+        elif input == 'M':
+            Medium = True
+        elif input == 'L':
+            Large = True
+        elif Small == True or Medium == True or Large == True:
+            SExit = True
+        else:
+            print("Please input a valid size")
+    # Displays prices for toppings
+    print("Now add toppings! every pizza already has sauce and cheese included")
+    print("Pepperoni = 1.99")
+    print("Sausage = 1.99")
+    print("Ham = 1.99")
+    print("Bell Peppers = 1.99")
+    print("Bacon = 2.99")
+    print("Olives = 2.99")
+    print("Anchovy = 2.99")
+    # Setting variables to check which toppings are added
+    TExit = False
+    Pepperoni = False
+    Olives = False
+    Sausage = False
+    Anchovy = False
+    BellPepper = False
+    Ham = False
+    Bacon = False
+    # Loop for adding toppings
+    while TExit == False:
+        toppings = input("What toppings would you like to add? (Type X to finish): ")
+        if toppings == 'Pepperoni':
+            Pepperoni = True
+        elif toppings == 'Sausage':
+            Sausage = True
+        elif toppings == 'Ham':
+            Ham = True
+        elif toppings == 'Bell Peppers':
+            BellPeppers = True
+        elif toppings == 'Bacon':
+            Bacon = True
+        elif toppings == 'Olives':
+            Olives = True
+        elif toppings == 'Anchovy':
+            Anchovy = True
+        elif toppings == 'X':
+            TExit = True
+        else:
+            print("Please input a valid topping or exit")
+    # Amount of pizzas
+    Amount = input("Please select the amount of pizza's you would like!: ")
+    # Concluding the order and allowing the option to go to a different area
+    print("Please select the cart to see your order with the price, or checkout when you feel read!")
+    OrderPlaced = True
+
 def view_cart():
-     #code here for programming lead
+    #Setting up price variables for all of the options
+    SmallPrice = 5.99
+    MediumPrice = 7.99
+    LargePrice = 9.99
+    PepperoniPrice = 1.99
+    SausagePrice = 1.99
+    HamPrice = 1.99
+    BellPeppersPrice = 1.99
+    BaconPrice = 2.99
+    OlivesPrice = 2.99
+    AnchovyPrice = 2.99
+    Total = 0.00
+    # Checking for pizza size and adding to total
+    if Small == True: 
+        print("Small = 5.99")
+        Total += SmallPrice
+    elif Medium == True: 
+        print("Medium = 7.99")
+        Total += MediumPrice
+    elif Large == True:
+        print("Large = 9.99")
+        Total += LargePrice
+    else:
+        print("Please place an order first!")
+        main_menu()
+    #checking for toppings and adding to total
+    if Pepperoni == True:
+        print("+ 1.99 (Pepperoni)")
+        Total += PepperoniPrice
+    elif Olives == True:
+        print("+ 2.99 (Olives)")
+        Total += OlivesPrice
+    elif Sausage == True:
+        print("+ 1.99 (Sausage)")
+        Total += SausagePrice
+    elif Anchovy == True:
+        print("+ 2.99 (Anchovy)")
+        Total += AnchovyPrice
+    elif BellPeppers == True:
+        print("+ 1.99 (BellPepper)")
+        Total += BellPeppersPrice
+    elif Ham == True:
+        print("+ 1.99 (Ham)")
+        Total += HamPrice
+    elif Bacon == True:
+        print("+ 2.99 (Bacon)")
+        Total += BaconPrice
+    # Multiplying by amount of pizza's
+    Total = Total * Amount
+    print(Amount, " Pizza's")
+    print("Your total is:", Total)
+    print("Please proceed to checkout if you would like to confirm this order!")
+
 def checkout():
-     #code here for programming lead
-def main_menu();
-    while True:
+    CheckingOut = input("Your total is $", Total, ", Would you like to Checkout? (Yes or No): ")
+    if input == 'Yes':
+        print("Thanks for shopping with us! Your order should arrive soon")
+    elif input == 'No':
+        print("If you would like to check your order again, View your cart")
+
+
+def main_menu():
+    Mexit = False
+    while MExit == False:
         print("PIZZA ORDERING MENU")
         print("1. Order Pizza")
         print("2. View Cart")
@@ -17,16 +142,24 @@ def main_menu();
     or they will get a message prompting them to pick from the options. based on the number
     they pick, they will get the menu of pizzas, checkout, see their cart, or exit the menu.
     """
+    choice = 0
     choice = input("Choose an option 1-4")
     if choice =="1":
         order_pizza()
     elif choice =="2":
-        view_cart()
+        if OrderPlaced == True:
+            view_cart()
+        else:
+            print("Please place an order first!")
     elif choice =="3":
-        Checkout()
-    elif choice =="4"
-        break
+        if OrderPlaced == True:
+            checkout()
+        else:
+            print("Please place an order first!")
+    elif choice =="4":
+        print("Goodbye, Have a great day!")
+        Mexit = True
     else:
         print("Please pick a Choice from 1 to 4")
-
-        # checking
+OrderPlaced = False
+main_menu()
